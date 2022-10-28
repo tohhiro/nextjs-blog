33ee59a1cc7fa5348473d194509a2bd74409a7e3
藤村広人 <fujimurahiroto@fujimurahirohitonoMacBook-Air.local>
2022-10-28 12:57:27 +0900

    change strings

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Tohhiro';
+const name = 'tohhiro';
 export const siteTitle = 'Next.js Tohhiro Sample Page';
 
 export default function Layout({ children, home }) {
